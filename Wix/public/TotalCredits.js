27c724ae25e758a75488f1ea0c601fabe8b2c2a5
Ashank-Dsouza <ashankdsouza054@yahoo.com>
2022-01-30 19:46:05 +0530

    adds integration with backend and mocking

--- Wix/public/TotalCredits.js
+++ Wix/public/TotalCredits.js
@@ -1,24 +1,51 @@
 const { isNaN } = require("lodash");
 import { GetQuarters } from 'public/Storage';
+import wixUsers from 'wix-users';
 
-export function GetTotalCredits(){
-    // the below gets 
-    var QuartersData  = GetQuarters(); //gets quarter data from session storage. It was stored in session storage after an API click.
-    var Quarters = QuartersData.Quarters;
+var IsMockOn = true;
+var MockUserID = "a2a70d73-8200-49e9-bfa6-eb14f775677b";
+
+function GetCurrentUserId() {
+    let user = wixUsers.currentUser;
+    let userId = user.id;
+    if (IsMockOn) {
+        userId = MockUserID
+    }
+    return userId;
+}
+
+export async function GetTotalCredits(){
+    var url = "https://s3ezx1ppdc.execute-api.us-east-1.amazonaws.com/dev/tax/getTaxCredits?userID="
+    url = url + GetCurrentUserId();
+    console.log("the url is: ", url);
+    const headers= {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    
+    var response  = await fetch(url, { method: 'GET', headers: headers });
+    var jsonData = await response.json();
+    console.log("the jsonData", jsonData)
+    var taxDetailsStr = jsonData.taxCreditsDetails;
+    console.log("the taxDetailsStr", taxDetailsStr);
+    var taxDetails = await  JSON.parse(taxDetailsStr);
+    var Quarters = taxDetails;
     if(!Quarters){
         null
     }
-
+    
     var totalCredits = 0;
-    for (const key in Quarters) {
-        if (Object.hasOwnProperty.call(Quarters, key)) {
-            const quarter = Quarters[key];
-            if(quarter.total_credits && !isNaN( quarter.total_credits)){ // adds credits only if it is a number
-                totalCredits = totalCredits + quarter.total_credits;
-            }
+
+    for (let index = 0; index < Quarters.length; index++) {
+        const quarter = Quarters[index];
+        if(quarter.total_credits && !isNaN( quarter.total_credits)){ // adds credits only if it is a number
+            totalCredits = totalCredits + quarter.total_credits;
         }
     }
     return totalCredits;
 } 
+
+
+
 
 
